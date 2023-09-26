b79c94efc5cc45ac590e96a908242508b4934800
mhmad-nr <83580680+mhmad-nr@users.noreply.github.com>
2023-09-26 13:08:46 +0330

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Canvas } from '@react-three/fiber'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Canvas>
+
+      </Canvas>
+    </>
+  )
+}
+
+export default App
